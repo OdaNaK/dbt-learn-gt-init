9fa89bcb92b1784531102976bd5708e763cf2023
OdaNaK <oumarba56@gmail.com>
2025-12-24 18:06:52 +0100

    sources added

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,14 +1,14 @@
 with customers as (
 
     select *
-    from {{ref('stg_jaffle_shop_customers')}}
+    from {{ref('stg_jaffle_shop__customers')}}
 
 ),
 
 orders as (
 
     select *
-    from {{ref('stg_jaffle_shop_orders')}}
+    from {{ref('stg_jaffle_shop__orders')}}
 ),
 
 customer_orders as (
