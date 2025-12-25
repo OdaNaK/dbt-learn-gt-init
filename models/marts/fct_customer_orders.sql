2667ae1554604d2cc484d75f487c082b64ad4ac9
OdaNaK <oumarba56@gmail.com>
2025-12-25 18:49:51 +0100

    CTE groupings

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,30 +1,46 @@
-select
+-- Import CTEs
 
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    last_name as surname,
-    first_name as givenname,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    round(amount/100.0,2) as order_value_dollars,
-    orders.status as order_status,
-    payments.status as payment_status
+with dbt_tutorial_customers as (
+    select * from {{source('jaffle_shop','customers')}}
+),
 
-from {{source('jaffle_shop','orders')}} as orders
+dbt_tutorial_orders as (
+    select * from {{source('jaffle_shop','orders')}}
+),
 
-join (
+dbt_tutorial_payments as(
+    select * from {{source('stripe','payment')}}
+),
 
+
+-- Logical CTEs
+
+customers as (
     select
         first_name || ' ' || last_name as name,
         *
+    from dbt_tutorial_customers
+),
 
-    from {{source('jaffle_shop','customers')}}
+a as (
 
-) customers
-on orders.user_id = customers.id
+    select
+        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
+        *
+    from dbt_tutorial_orders
+),
 
-join (
+b as (
+
+    select
+        first_name || ' ' || last_name as name,
+            *
+    from dbt_tutorial_customers
+
+),
+
+customer_order_history as (
+
     select
         b.id as customer_id,
         b.name as full_name,
@@ -85,37 +101,54 @@ join (
 
         array_agg(distinct a.id) as order_ids
 
-    from (
+    from  a
 
-        select
-            row_number() over (partition by user_id order by order_date, id) as user_order_seq,
-            *
-        from {{source('jaffle_shop','orders')}}
-
-    ) a
-
-    join (
-
-        select
-            first_name || ' ' || last_name as name,
-            *
-        from {{source('jaffle_shop','customers')}}
-
-    ) b
+    join  b
 
     on a.user_id = b.id
 
-    left outer join {{source('stripe','payment')}} c
+    left outer join dbt_tutorial_payments c
     on a.id = c.orderid
 
     where a.status not in ('pending') and c.status != 'fail'
 
     group by b.id, b.name, b.last_name, b.first_name
 
-) customer_order_history
-on orders.user_id = customer_order_history.customer_id
+),
 
-left outer join {{source('stripe','payment')}} payments
-on orders.id = payments.orderid
 
-where payments.status != 'fail'
+-- Final CTEs
+
+final as (
+    select
+
+        orders.id as order_id,
+        orders.user_id as customer_id,
+        last_name as surname,
+        first_name as givenname,
+        first_order_date,
+        order_count,
+        total_lifetime_value,
+        round(amount/100.0,2) as order_value_dollars,
+        orders.status as order_status,
+        payments.status as payment_status
+
+    from dbt_tutorial_orders as orders
+
+    join customers
+    on orders.user_id = customers.id
+
+    join  customer_order_history
+    on orders.user_id = customer_order_history.customer_id
+
+    left outer join dbt_tutorial_payments payments
+    on orders.id = payments.orderid
+
+    where payments.status != 'fail'
+    )
+  
+    -- Final Select
+
+    select
+    *
+    from final
